3f660f1a4f2645a49cfaaa4f76a60133406b4a07
megakasmin <meghakasmin99@gmail.com>
2019-12-16 09:40:46 +0700

    add Team

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router , Route, Switch } from 'react-router-dom'
-import  { GuestHeader , Login , Team } from "./components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { GuestHeader, Login, Team } from "./components";
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
         <Router>
-          <GuestHeader/>
+          <GuestHeader />
           <Switch>
-              <Route>
+            <Route>
               <Route path="/" exact={true}>
                 <Team />
               </Route>
-                <Route path="/login" >
-                  <Login />
-                </Route>  
+              <Route path="/login">
+                <Login />
               </Route>
+            </Route>
           </Switch>
         </Router>
       </div>
