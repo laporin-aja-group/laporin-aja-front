e7acd48c208c498027d7858a2a5e5a759dc6314d
megakasmin <meghakasmin99@gmail.com>
2019-12-17 10:36:11 +0700

    add and change something

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -21,26 +21,28 @@ class Landing extends Component {
       <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing-grid">
           <Cell col={12}>
+            <h3 className="text">Welcome</h3>
             <img src="./logo-hafiz1.png" alt="avatar" className="avatar" />
 
             <div className="banner-text">
-              <h5>
+              <h6>
                 LaporinAja! A social application created specifically to assist
                 the Indonesian community in reporting problems around their
                 environment. By using this application, people can save time and
                 energy. This application makes it easy to provide reports
                 remotely and will be immediately followed up if it has been
                 approved.
-              </h5>
+              </h6>
               <Button raised colored>
-                <a href="./Login.js">Get Started</a>
+                <a href="./Team.js">Get Started</a>
               </Button>
 
               <hr />
 
               <p>
-                HTML/CSS | JavaScript | React | React-mdl | Material-ui | NodeJS
-                | Express | MongoDB | Mongoose |
+                HTML/CSS | JavaScript | React | React-Dom | React-mdl |
+                React-Bootstrap | Material-ui | NodeJS | Express | MongoDB |
+                Mongoose | Sweetalert2 |
               </p>
             </div>
           </Cell>
