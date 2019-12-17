e6922a1cc917fba7ed4487bc777aaa005ed83593
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2019-12-17 19:11:43 +0700

    add edit file

--- src/components/Data Report/User/EditProblem.js
+++ src/components/Data Report/User/EditProblem.js
@@ -1,231 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { withRouter } from "react-router-dom";
-import { Formik, ErrorMessage } from "formik";
+import React, { Component } from 'react'
+import { withRouter, Link } from 'react-router-dom'
+import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import RegisterLogo from "../../Guest/banner.png"
-import Swal from "sweetalert2"
-import ReactFilestack from 'filestack-react';
-import { verify, axiosReportsUsers } from "../../helpers"
+import Typography from '@material-ui/core/Typography';
+import { axiosReportsUsers } from '../../helpers';
 
-const useStyles = makeStyles(theme => ({
-    "@global": {
-        body: {
-            backgroundColor: theme.palette.common.white
-        }
-    },
-    paper: {
-        backgroundColor: "white",
-        border: "solid",
-        borderRadius: "3%",
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(1)
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-        width: "50%",
-        marginLeft: "25%",
-        display: "flex",
-        alignItems: "center"
-    },
-    filestack: {
-        margin: theme.spacing(3, 0, 2),
-        width: "30%",
-        display: "flex",
-        alignItems: "center"
+class EditProblem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data : []
+        };
     }
 
-}));
+    showData = () => {
+        const {
+            match: {
+                params: { id }
+            }
+        } = this.props;
+        
 
-
-function EditProblem(props) {
-
-    const [data, setData] = useState([])
-    const idProblem = props.match.params.id
-    
-    useEffect(() => {
-      axiosReportsUsers()
-            .get(`/report-users/id/${idProblem}`)
+        axiosReportsUsers()
+            .get(`report-users/id/${id}`)
             .then(response => {
-              if (response.status === 200) {
-                setData({data : response.data.data}) 
-              }
+                this.setState({ data: response.data.data});
+                
             })
-    },[])
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-    const dataThis = Array.isArray(data.data)&&data.data[0]
-    const problemThis = dataThis.problem;
-    let problemMy = "Kanan";
-    console.log(problemMy);
-    
-    
+    componentDidMount = () => {
+        this.showData();
+    }
 
-    const classes = useStyles();
-    let urlLoginLive = process.env.REACT_APP_API_LOGIN_LIVE;
-    let fileStack = process.env.REACT_APP_API_KEY_FILESTACK;
-    
-
+    render() {
+        console.log(this.state.data);
+        
         return (
-            <Container component="main" maxWidth="xs">
-              <CssBaseline />
-              <div className={classes.paper}>
-                    <img src={RegisterLogo} alt="register" id="Login-Image"/>
-                <Formik
-                initialValues={{
-                  problem: "",
-                  location: "",
-                  description: ""
-                }}
-                enableReinitialize={true}
-                validate=""
-                onSubmit={values => {
-                  
-                  if (values.problem === "" || values.location === "" || values.description === "" || values.image === null) {
-                    Swal.fire({
-                      icon: 'error',
-                      title: "Make sure your report is filled in correctly!",
-                    })
-                  } else {
-                  axiosReportsUsers()
-                    .post(`${urlLoginLive}report-users`, {...values, user : verify()._id})
-                    .then(response => {
-                      if (response.status === 200) {
-                        Swal.fire({
-                          icon: 'success',
-                          title: 'Add Report Successfully',
-                          text: 'Now you can check your report',
-                        }).then(response => {
-                          props.history.push("/report-users")
-                        })
-                      } else {
-                        Swal.fire({
-                          icon: 'error',
-                          title: 'There`s something error when you add report, please add report again'
-                        })
-                      }
-                    })
-                  }
-                }}
-                >
-                  {({
-                    values,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    setFieldValue,
-                    isSubmitting
-                  }) => (
-                <form 
-                className={classes.form}
-                onSubmit={handleSubmit}
-                noValidate
-                style={{ margin:"0px", padding:"10px 30px 30px"}}
-                >
-                    <Grid item xs={12}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="problem"
-                        label="Problem"
-                        name="problem"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        defaultValue={problemMy}
-                        autoComplete="problem"
-                      />
-                       <p></p>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="location"
-                        label="Detail Location"
-                        name="location"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        defaultValue={values.location}
-                        autoComplete="location"
-                        multiline
-                      />
-                       <p
-                      style={{
-                        color:"red",
-                        fontStyle:"italic"
-                      }}
-                      >
-                        <ErrorMessage name="location" />
-                      </p>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                      <TextField
-                        variant="outlined"
-                        rows="4"
-                        required
-                        fullWidth
-                        id="description"
-                        label="Description (Optional)"
-                        name="description"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        defaultValue={values.description}
-                        autoComplete="description"
-                        multiline
-                      />
-                       <p>
-                      </p>
-                    </Grid>
-
-                    <img src={dataThis.image} alt="data_gambar" style={{width:"100%"}}/>
-
-                    <ReactFilestack
-                    apikey={fileStack}
-                    actionOptions ={{
-                      accept: ["image/*", "video/*"]
-                    }}
-                    onSuccess={(res) => {
-                      setFieldValue(
-                        "image", res.filesUploaded[0].url
-                      );
-                    }}
-                    componentDisplayMode={{
-                        type: 'button',
-                        customText: 'Choose File',
-                        customClass: classes.filestack
-                    }}
-                    /><a>{problemMy}</a>
-
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                  >
-                    Submit
-                  </Button>
-
-                </form>
-                )}
-                </Formik>
-              </div>
-            </Container>
-          );   
+            <div>
+                <Paper style={{marginTop:"20px", marginLeft:"20%", marginRight:"20%"}}>
+                    
+                </Paper>
+            </div>
+        )
+    }
 }
 
 export default withRouter(EditProblem)
-
