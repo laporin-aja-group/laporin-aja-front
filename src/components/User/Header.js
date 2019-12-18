e6a0f2390123144213d25e1834bfd079752fe6dc
Hafiz Yanuar Ibrahim <hafizyanuar95@gmail.com>
2019-12-18 10:47:18 +0700

    add new view

--- src/components/User/Header.js
+++ src/components/User/Header.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Main from './Main'
 import swal from 'sweetalert2'
 import { verify } from '.././helpers'
+import { Badge } from 'react-bootstrap'
 
 const logOut = () => {
   localStorage.removeItem("token");
@@ -29,8 +30,11 @@ export default class Header extends Component {
             <Typography  variant="h6">
                 <Main />
             </Typography>
-            <Typography  variant="h6" style={{ marginLeft:"10px", color:"Black"}}>
-                You are loged in as "{verify().role}"
+            <Typography  
+            variant="h6" 
+            style={{ marginLeft:"10px", fontSize:"24px"}}
+            >
+              <Badge variant="info">You are loged in as <Badge variant="primary">{verify().role}</Badge></Badge>
             </Typography>
             <Typography
               variant="h6"
