a19e9dbd8a488612d3639464a6a11dfa4666b75e
Apriadi <apriadim>
2019-12-18 12:45:42 +0700

    add edit report

--- src/components/Data Report/User/EditProblem.js
+++ src/components/Data Report/User/EditProblem.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper';
+import RegisterLogo from "../../Guest/banner.png"
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 import { axiosReportsUsers } from '../../helpers';
+import Grid from '@material-ui/core/Grid';
 
 class EditProblem extends Component {
     constructor(props) {
@@ -42,8 +44,72 @@ class EditProblem extends Component {
         
         return (
             <div>
-                <Paper style={{marginTop:"20px", marginLeft:"20%", marginRight:"20%"}}>
-                    
+                <Paper style={{marginTop:"20px", marginLeft:"30%", marginRight:"30%"}}>
+                    <img src={RegisterLogo} alt="register" id="Login-Image"/>
+                    <div style={{padding:"20px"}}>
+                    <Grid item xs={12}>
+                      <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="problem"
+                        label="Problem"
+                        name="problem"
+                        autoComplete="problem"
+                      />
+                    </Grid>
+                    </div>
+                    <div style={{padding:"20px"}}>
+                    <Grid item xs={12}>
+                      <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="location"
+                        label="Detail Location"
+                        name="location"
+                        autoComplete="location"
+                        multiline
+                      />
+                    </Grid>
+                    </div>
+                    <div style={{padding:"20px"}}>
+                    <Grid item xs={12}>
+                      <TextField
+                        variant="outlined"
+                        rows="4"
+                        required
+                        fullWidth
+                        id="description"
+                        label="Description (Optional)"
+                        name="description"
+                        autoComplete="description"
+                        multiline
+                      />
+                    </Grid>
+                    </div>
+
+                    <div style={{padding:"20px"}}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Back
+                        </Button>
+                    </div>
+
+                    <div style={{padding:"20px"}}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Submit
+                        </Button>
+                    </div>
                 </Paper>
             </div>
         )
